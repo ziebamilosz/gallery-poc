e9838d9e79e9b50d5a7fffaa0c23cd0469c25bf9
ziebamilosz <zieba.milosz.k@gmail.com>
2021-10-11 17:13:49 +0200

    Generate project

---
+++ src/main/java/com/example/gallerypoc/GalleryPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.gallerypoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalleryPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalleryPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gallerypoc/GalleryPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gallerypoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalleryPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
